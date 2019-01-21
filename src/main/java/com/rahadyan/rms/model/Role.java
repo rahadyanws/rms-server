0a9d44caf9b3b7517ae3ca60e18d96ff4c1de062
Rahadyan Widhi Saktiaji <Rahadyan_W995@mitrais.com>
2019-01-21 15:09:40 +0700

    adding model class

---
+++ src/main/java/com/rahadyan/rms/model/Role.java
@@ -0,0 +1,27 @@
+package com.rahadyan.rms.model;
+
+import org.hibernate.annotations.NaturalId;
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+}
